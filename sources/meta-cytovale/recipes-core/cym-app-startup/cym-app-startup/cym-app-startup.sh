e59e4378ffd23154268a30393608102d9b12ac4d
Adam <PseudoDesign@users.noreply.github.com>
2019-04-09 16:35:56 -0600

    cleaned up startup

--- sources/meta-cytovale/recipes-core/cym-app-startup/cym-app-startup/cym-app-startup.sh
+++ sources/meta-cytovale/recipes-core/cym-app-startup/cym-app-startup/cym-app-startup.sh
@@ -4,16 +4,9 @@ export PYTHONUNBUFFERED=1
 
 cd ${APP_ROOT_DIR}/cytovale_app
 
-python3 cytovale_django/manage.py migrate
-python3 cytovale_django/manage.py runserver 0.0.0.0:8000 &
-
-# TODO: Test if server is still running
-
-touch /tmp/cym-webserver-running
+echo "Running"
 
 while true; do
   systemd-notify READY=1
   sleep 60
 done
-
-
